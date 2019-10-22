61311a14b2e4c379b89287d273969c51f1d5f20d
itadmin <satish.agarkar@successive.tech>
2019-10-22 15:37:13 +0530

    day 10 task completed

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,11 @@
 import * as express from 'express';
 
-import {notfount_middleware} from './../libs/routes/notFoundRoute';
-import {error_middleware} from './../libs/routes/errorHandler';
+import { notfount_middleware } from './../libs/routes/notFoundRoute';
+import { error_middleware } from './../libs/routes/errorHandler';
 
-import {IConfig} from './config/IConfig';
+import { IConfig } from './config/IConfig';
 
 import router from './router'
-
 
 var bodyParser = require('body-parser');
 
@@ -17,15 +16,15 @@ export class Server {
         this.app = express();
         this.PORT = config.PORT;
     }
-    
+
     bootstrap() {
         this.run();
         this.setupRoutes();
         this.initBodyParser();
-        
-        
-        
-        this.app.get('/health-check', (req : express.Request, res: express.Response) => {
+
+
+
+        this.app.get('/health-check', (req: express.Request, res: express.Response) => {
             res.status(200).json({
                 msg: 'I am OK'
             });
@@ -34,24 +33,26 @@ export class Server {
 
         });
         this.app.use('/api', router);
+        this.app.use(notfount_middleware);
+        this.app.use(error_middleware)
     }
     run() {
         try {
-            this.app.listen( this.PORT, () => {
+            this.app.listen(this.PORT, () => {
                 console.log(`server is up and listening on ${this.PORT}`);
             })
-        } catch(error){
+        } catch (error) {
             console.log(error);
         }
     }
     setupRoutes() {
-        
+
 
     }
     private initBodyParser() {
         this.app.use(bodyParser.urlencoded({ extended: false }));
-    
+
         this.app.use(bodyParser.json());
-      }
-} 
+    }
+}
 // module.exports = router;
